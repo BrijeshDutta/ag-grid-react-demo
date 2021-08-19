49381f661ffadb5d26b285160929f69806fbba09
BrijeshDutta <brijeshdtt@gmail.com>
2021-08-19 09:35:43 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { render } from 'react-dom';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const App = () => {
+  useEffect(() => {
+    console.log('use effect called..');
+  }, []);
+  const rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
+
+  return (
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <AgGridReact rowData={rowData}>
+        <AgGridColumn field="make" />
+        <AgGridColumn field="model" />
+        <AgGridColumn field="price" />
+      </AgGridReact>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
